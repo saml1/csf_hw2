70a257e9f12f37ca3a55656cf21e06fef3168eb2
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-09-23 16:42:14 -0400

    added basic untested fuctionality to main

--- c_hexmain.c
+++ c_hexmain.c
@@ -6,11 +6,24 @@
 int main(void) {
     char buf_string[16]; //string from std in
     char buf_offset[8]; //string of offset
+    char byte_in_hex[2]; //string of hex value of char
     long offset_count = 0;
-    while(hex_read(buf_string) > 0){ //while something is getting read in
+    long chars_read = 0;
+    chars_read = hex_read(buf_string);
+    while(chars_read > 0){ //while something is getting read in
         hex_format_offset(offset_count, buf_offset);
         hex_write_string(buf_offset);
         hex_write_string(": ");
+        for(int i = 0; i < 16; i++){
+            if( i < chars_read){
+                hex_format_byte_as_hex(buf_string[i], byte_in_hex);
+                hex_write_string(byte_in_hex);
+                hex_write_string(" ");
+            } else{
+                hex_format_byte_as_hex("   ");
+            }
+        }
+        chars_read = hex_read(buf_string);
         offset_count++;
     }
 
