d1bf000e5dec7ab0a729f9f8f49cc2a443e20862
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-09-23 16:24:31 -0400

    started working on main

--- c_hexmain.c
+++ c_hexmain.c
@@ -1,14 +1,16 @@
 // C implementation of hexdump main function
 
 #include "hexfuncs.h"  // this is the only header file which may be included!
-#include <stdlib.h> //REMOVE THIS (ONLY FOR TESTING)
 
 
 int main(void) {
-    char * test = (char *) malloc(4*sizeof(char));
-    long a = hex_read(test);
-    hex_write_string(test);
-   
+    char [16] buf_string; //string from std in
+    char [8] buf_offset; //string of offset
+    long offset_count = 0;
+    while(hex_read(buf_string) > 0){ //while something is getting read in
+        hex_format_offset(offset_count, buf_offset);
+        hex_write_string(buf_offset);
+    }
 
 }
 
