3433a0ab237a125e547d1b0418301c6e152d47f1
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-09-24 13:59:18 -0400

    print debug

--- c_hexmain.c
+++ c_hexmain.c
@@ -10,7 +10,8 @@ int main(void) {
     char byte_in_hex[2]; //string of hex value of char
     long offset_count = 0;
     long chars_read = 0;
-    chars_read = hex_read(buf_string);
+    chars_read = hex_read(buf_string);//stores input in buf_string and records #bytes in chars_read;
+    printf("buf_string[0]: %c\n", buf_string[0]);
     while(chars_read > 0){ //while something is getting read in
         hex_format_offset(offset_count, buf_offset); //storing string-rep of offset_count in buf_offset
         hex_write_string(buf_offset); //printing offset
@@ -19,7 +20,7 @@ int main(void) {
             if( i < chars_read){
                 hex_format_byte_as_hex(buf_string[i], byte_in_hex);
                 hex_write_string(byte_in_hex);
-                printf("byte_in_hex: %s\n", byte_in_hex);
+                //printf("byte_in_hex: %s\n", byte_in_hex);
                 hex_write_string(" ");
             } else{
                 hex_write_string("   ");
