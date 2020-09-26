ca6b11651e50f1cbc56f6294d52d54f02194b55a
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-09-26 17:45:32 -0400

    initialized overflow_element to -1 at the start and made checks to see if != -1 to do stuff with it

--- c_hexmain.c
+++ c_hexmain.c
@@ -14,19 +14,19 @@ int main(void) {
     long chars_read = 0;
     long chars_read_next = 0;
     long chars_read_total = 0;
-    int overflow_element = 0;
+    int overflow_element = -1;
     chars_read = hex_read(buf_string_final); //stores input in buf_string and records #bytes in chars_read;
     //printf("chars_read")
     while(chars_read != 0){
         chars_read_total = chars_read + chars_read_next - overflow_element;//taking into account overflow
-        //if(overflow_element != 0){ //not on first loop
+        if(overflow_element != -1){ //not on first loop
             for(int i = overflow_element; i < chars_read_next; i++){//storing prev overflow vals
                 buf_string_final[i-overflow_element] = buf_string_next[i];
             }
             for(int i = overflow_element; i < chars_read_next; i++){//storing new chars in buf_string_final
                 buf_string_final[i] = buf_string[i-overflow_element];
             }
-        //}
+        }
         while(chars_read_total < 16){ //keeps reading input until it gets to 16 chars
             chars_read_next = hex_read(buf_string_next); //stores input in buf_string_next
             chars_read_total += chars_read_next;
@@ -61,30 +61,32 @@ int main(void) {
         offset_count+=16;
         hex_write_string("\n");
     }
-    hex_format_offset(offset_count, buf_offset); //storing string-rep of offset_count in buf_offset
-    hex_write_string(buf_offset); //printing offset
-    hex_write_string(": ");
-    for(int i = overflow_element; i < 16+overflow_element; i++){
-        if(i < chars_read_next){
-            hex_format_byte_as_hex(buf_string_next[i], byte_in_hex);
-            hex_write_string(byte_in_hex);
-            hex_write_string(" ");
-        }else{
-            hex_write_string("   ");
+    if(overflow_element != -1){
+        hex_format_offset(offset_count, buf_offset); //storing string-rep of offset_count in buf_offset
+        hex_write_string(buf_offset); //printing offset
+        hex_write_string(": ");
+        for(int i = overflow_element; i < 16+overflow_element; i++){
+            if(i < chars_read_next){
+                hex_format_byte_as_hex(buf_string_next[i], byte_in_hex);
+                hex_write_string(byte_in_hex);
+                hex_write_string(" ");
+            }else{
+                hex_write_string("   ");
+            }
         }
+        hex_write_string(" ");
+        for(int i = overflow_element; i < chars_read_next; i++){
+            buf_string_next[i] = hex_to_printable(buf_string_next[i]);
+        }
+        buf_string_next[chars_read_next] = '\0';
+        //printf("buf_string_next: %s\n", buf_string_next);
+        char buf_string_end[chars_read_next-overflow_element+1];
+        for(int i = overflow_element; i < chars_read_next; i++){
+            buf_string_end[i-overflow_element] = buf_string_next[i];
+        }
+        buf_string_end[chars_read_next-overflow_element] = '\0';
+        hex_write_string(buf_string_end);
+        hex_write_string("\n");
     }
-    hex_write_string(" ");
-    for(int i = overflow_element; i < chars_read_next; i++){
-        buf_string_next[i] = hex_to_printable(buf_string_next[i]);
-    }
-    buf_string_next[chars_read_next] = '\0';
-    //printf("buf_string_next: %s\n", buf_string_next);
-    char buf_string_end[chars_read_next-overflow_element+1];
-    for(int i = overflow_element; i < chars_read_next; i++){
-        buf_string_end[i-overflow_element] = buf_string_next[i];
-    }
-    buf_string_end[chars_read_next-overflow_element] = '\0';
-    hex_write_string(buf_string_end);
-    hex_write_string("\n");
 }
 
