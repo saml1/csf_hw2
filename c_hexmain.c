2bbf780582a03107f8ba1176b389ec2b0546a3ca
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-09-27 11:16:34 -0400

    pd

--- c_hexmain.c
+++ c_hexmain.c
@@ -76,9 +76,9 @@ int main(void) {
         }
         buf_string_final[chars_read_total] = '\0';
         hex_write_string(buf_string_final);
-        printf("chars_read_total: %lx\n", chars_read_total);
-        printf("chars_read: %lx\n", chars_read);
-        printf("chars_read_next: %lx\n", chars_read_next);
+        printf("chars_read_total: %ld\n", chars_read_total);
+        printf("chars_read: %ld\n", chars_read);
+        printf("chars_read_next: %ld\n", chars_read_next);
         printf("buf_string_final: %s\n", buf_string_final);
         if(chars_read_next == 0 && readNext == 1){
             hex_write_string("\n");
