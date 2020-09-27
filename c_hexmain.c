66d86e9cf47c9565e28b74fa172e1c60c5e16aed
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-09-27 15:55:02 -0400

    added if chars wont get to 16

--- c_hexmain.c
+++ c_hexmain.c
@@ -21,25 +21,22 @@
             while(chars_in_final < 16){
                 chars_read = hex_read(buf_string); //read input to buf_string, store in chars_read
                 if(chars_in_final + chars_read >= 16){
-                    for(int i = chars_in_final; i < 16; i++){
+                    for(int i = chars_in_final; i < 16; i++){//storing chars from buf_string in buf_string_final
                         buf_string_final[i] = buf_string[i-chars_in_final];
                     }
-                    printf("1buf_string_final: %s\n", buf_string_final);
-                    printf("1buf_string: %s\n", buf_string);
-                    printf("1chars_read: %ld\n", chars_read);
-                    printf("1chars_in_final: %ld\n", chars_in_final);
                     for(int i = 0; i < chars_read+chars_in_final-16; i++){//moving unused chars to front
                         buf_string[i] = buf_string[16-chars_in_final+i];
                     }
                     chars_read = chars_read+chars_in_final-16; //amount of chars "left over"
                     buf_string[chars_read] = '\0';
                     chars_in_final = 16;
+                }else{ //if still added up won't get to 16
+                    for(int i = 0; i < chars_read; i++){
+                        buf_string_final[chars_in_final + i] = buf_string[i];
+                    }
+                    chars_in_final += chars_read;
                 }
             }
-            printf("buf_string_final: %s\n", buf_string_final);
-            printf("buf_string: %s\n", buf_string);
-            printf("chars_read: %ld\n", chars_read);
-            printf("chars_in_final: %ld\n", chars_in_final);
         }
     }
 
