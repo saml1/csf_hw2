0d8489aa9bb8c6e330798c4c14f1ce9717a51591
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-09-27 09:18:33 -0400

    commented out pd

--- c_hexmain.c
+++ c_hexmain.c
@@ -16,8 +16,8 @@ int main(void) {
     long chars_read_total = 0;
     int overflow_element = -1;
     chars_read = hex_read(buf_string_final); //stores input in buf_string and records #bytes in chars_read;
-    printf("chars_read: %lx\n", chars_read);
-    printf("buf_string_final: %s\n", buf_string_final);
+    //printf("chars_read: %lx\n", chars_read);
+    //printf("buf_string_final: %s\n", buf_string_final);
     while(chars_read != 0){
         if(overflow_element!= -1){
             chars_read_total = chars_read + chars_read_next - overflow_element;//taking into account overflow
