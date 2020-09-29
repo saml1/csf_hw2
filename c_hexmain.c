987037aee302e6fd0609c24b0d188113af2051cb
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-09-29 11:12:12 -0400

    changed chars_read !=0 to >0 for outer while loop

--- c_hexmain.c
+++ c_hexmain.c
@@ -41,7 +41,7 @@
         }
         //buf_string[chars_read] = '\0';
 
-        while(chars_read != 0){ //While ! eof:
+        while(chars_read > 0){ //While ! eof:
             for(int i = 0; i < chars_read; i++){ //add chars from buf_string to buf_string_final and keep track of # chars with chars_in_final
                 if(chars_in_final != 16){
                     buf_string_final[i+chars_in_final] = buf_string[i];
