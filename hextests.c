026946b14fa8a72b9801ea4db09ae7b537796a8d
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-10-01 15:56:31 -0400

    pd

--- hextests.c
+++ hextests.c
@@ -50,6 +50,7 @@ void testFormatOffset(TestObjs *objs) {
   (void) objs; // suppress warning about unused parameter
   char buf[16];
   hex_format_offset(1L, buf);
+  printf("buf: %s\n", buf);
   ASSERT(0 == strcmp(buf, "00000001"));
 
   hex_format_offset(12345L, buf);
