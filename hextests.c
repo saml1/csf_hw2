ff5b4c12e6b069884da729195949ae8bb80d383f
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-10-02 11:37:15 -0400

    more tests

--- hextests.c
+++ hextests.c
@@ -50,12 +50,16 @@ void testFormatOffset(TestObjs *objs) {
   (void) objs; // suppress warning about unused parameter
   char buf[16];
   hex_format_offset(1L, buf);
-  printf("buf: %s\n", buf);
+  //printf("buf: %s\n", buf);
   ASSERT(0 == strcmp(buf, "00000001"));
 
   hex_format_offset(12345L, buf);
-  printf("buf: %s\n", buf);
+  //printf("buf: %s\n", buf);
   ASSERT(0 == strcmp(buf, "00003039"));
+
+  hex_format_offset(3497L, buf);
+    printf("buf: %s\n", buf);
+  ASSERT(0 == strcmp(buf, "00000da9"));
 }
 
 void testFormatByteAsHex(TestObjs *objs) {
